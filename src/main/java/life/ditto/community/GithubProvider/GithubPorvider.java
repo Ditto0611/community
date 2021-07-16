80ff995fc87f9f657d15205db0ff97a76eb76540
Ditto <1049800760@qq.com>
2021-07-16 17:27:42 +0800

    community

---
+++ src/main/java/life/ditto/community/GithubProvider/GithubPorvider.java
@@ -0,0 +1,52 @@
+package life.ditto.community.GithubProvider;
+
+import com.alibaba.fastjson.JSON;
+import life.ditto.community.dto.AccessTokenDTO;
+import life.ditto.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @Author Ditto
+ * @Date 2021/7/16 15:35
+ * @Version 1.0
+ */
+@Component
+public class GithubPorvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String s = response.body().string();
+            String token = s.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string,GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/life/ditto/community/controller/AuthorizeController.java
+++ src/main/java/life/ditto/community/controller/AuthorizeController.java
@@ -1,5 +1,9 @@
 package life.ditto.community.controller;
 
+import life.ditto.community.GithubProvider.GithubPorvider;
+import life.ditto.community.dto.AccessTokenDTO;
+import life.ditto.community.dto.GithubUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +15,20 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private GithubPorvider githubPorvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,@RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("2b3d20b60352de04f79a");
+        accessTokenDTO.setClient_secret("b099f73c6bd92d6290dd0fdf29b0e3d7cc1e6110");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8081/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubPorvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubPorvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
