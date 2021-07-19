c17bb3329af326e5d822716b1c6746b1db64428f
Ditto <1049800760@qq.com>
2021-07-19 15:43:25 +0800

    add sql ddl

--- src/main/java/life/ditto/community/GithubProvider/GithubPorvider.java
+++ src/main/java/life/ditto/community/GithubProvider/GithubPorvider.java
@@ -27,6 +27,7 @@ public class GithubPorvider {
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String s = response.body().string();
+            System.out.println(s);
             String token = s.split("&")[0].split("=")[1];
             return token;
         } catch (Exception e) {
@@ -38,7 +39,9 @@ public class GithubPorvider {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("https://api.github.com/user?access_token=" + accessToken)
+                .header("Authorization", "token " + accessToken)
                 .build();
+        System.out.println(accessToken);
         try {
             Response response = client.newCall(request).execute();
             String string = response.body().string();
