78df4cece05fdc63735db7922f956f75073e8eb0
Ditto <1049800760@qq.com>
2021-07-19 08:56:49 +0800

    'community'

--- src/main/java/life/ditto/community/controller/AuthorizeController.java
+++ src/main/java/life/ditto/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.ditto.community.GithubProvider.GithubPorvider;
 import life.ditto.community.dto.AccessTokenDTO;
 import life.ditto.community.dto.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +19,22 @@ public class AuthorizeController {
     @Autowired
     private GithubPorvider githubPorvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("{github.client.secret}")
+    private String clientSecret;
+    @Value("{github.redirect.uri}")
+    private String redirectUri;
+
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,@RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("2b3d20b60352de04f79a");
-        accessTokenDTO.setClient_secret("b099f73c6bd92d6290dd0fdf29b0e3d7cc1e6110");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8081/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubPorvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubPorvider.getUser(accessToken);
